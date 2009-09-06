c552f9de1f80743760e6e4a1d970f3feed81555e
Jean-Philippe Grimaldi <jeanphi@via.ecp.fr>
2009-09-06 11:25:33 +0200

    Backed out changeset a2134df55212

---
+++ textures/fbm.h
@@ -0,0 +1,86 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2009 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+// fbm.cpp*
+#include "lux.h"
+#include "texture.h"
+#include "paramset.h"
+
+// TODO - radiance - add methods for Power and Illuminant propagation
+
+namespace lux
+{
+
+// FBmTexture Declarations
+template <class T> class FBmTexture : public Texture<T> {
+public:
+	// FBmTexture Public Methods
+	virtual ~FBmTexture() {
+		delete mapping;
+	}
+	FBmTexture(int oct, float roughness, TextureMapping3D *map) {
+		omega = roughness;
+		octaves = oct;
+		mapping = map;
+	}
+	virtual T Evaluate(const TsPack *tspack, const DifferentialGeometry &dg) const {
+		Vector dpdx, dpdy;
+		Point P = mapping->Map(dg, &dpdx, &dpdy);
+		return FBm(P, dpdx, dpdy, omega, octaves);
+	}
+	
+	static Texture<float> * CreateFloatTexture(const Transform &tex2world, const TextureParams &tp);
+	static Texture<SWCSpectrum> * CreateSWCSpectrumTexture(const Transform &tex2world, const TextureParams &tp);
+	
+private:
+	// FBmTexture Private Data
+	int octaves;
+	float omega;
+	TextureMapping3D *mapping;
+};
+
+// FBmTexture Method Definitions
+template <class T> Texture<float> * FBmTexture<T>::CreateFloatTexture(const Transform &tex2world,
+		const TextureParams &tp) {
+	// Initialize 3D texture mapping _map_ from _tp_
+	TextureMapping3D *map = new IdentityMapping3D(tex2world);
+	// Apply texture specified transformation option for 3D mapping
+	IdentityMapping3D *imap = (IdentityMapping3D*) map;
+	imap->Apply3DTextureMappingOptions(tp);
+
+	return new FBmTexture<float>(tp.FindInt("octaves", 8),
+		tp.FindFloat("roughness", .5f), map);
+}
+
+template <class T> Texture<SWCSpectrum> * FBmTexture<T>::CreateSWCSpectrumTexture(const Transform &tex2world,
+		const TextureParams &tp) {
+	// Initialize 3D texture mapping _map_ from _tp_
+	TextureMapping3D *map = new IdentityMapping3D(tex2world);
+	// Apply texture specified transformation option for 3D mapping
+	IdentityMapping3D *imap = (IdentityMapping3D*) map;
+	imap->Apply3DTextureMappingOptions(tp);
+
+	return new FBmTexture<SWCSpectrum>(tp.FindInt("octaves", 8),
+		tp.FindFloat("roughness", .5f), map);
+}
+
+}//namespace lux
---
+++ textures/scale.h
@@ -0,0 +1,78 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2009 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+
+// scale.cpp*
+#include "lux.h"
+#include "texture.h"
+#include "paramset.h"
+
+namespace lux
+{
+
+// ScaleTexture Declarations
+template <class T1, class T2>
+class ScaleTexture : public Texture<T2> {
+public:
+	// ScaleTexture Public Methods
+	ScaleTexture(boost::shared_ptr<Texture<T1> > t1,
+			boost::shared_ptr<Texture<T2> > t2) {
+		tex1 = t1;
+		tex2 = t2;
+	}
+	virtual ~ScaleTexture() { }
+	virtual T2 Evaluate(const TsPack *tspack, const DifferentialGeometry &dg) const {
+		return tex1->Evaluate(tspack, dg) * tex2->Evaluate(tspack, dg);
+	}
+	virtual void SetPower(float power, float area) {
+		// Update sub-textures
+		tex1->SetPower(power, area);
+		tex2->SetPower(power, area);
+	}
+	virtual void SetIlluminant() {
+		// Update sub-textures
+		tex1->SetIlluminant();
+		tex2->SetIlluminant();
+	}
+	static Texture<float> * CreateFloatTexture(const Transform &tex2world, const TextureParams &tp);
+	static Texture<SWCSpectrum> * CreateSWCSpectrumTexture(const Transform &tex2world, const TextureParams &tp);
+private:
+	boost::shared_ptr<Texture<T1> > tex1;
+	boost::shared_ptr<Texture<T2> > tex2;
+};
+
+// ScaleTexture Method Definitions
+template <class T, class U> inline Texture<float> * ScaleTexture<T,U>::CreateFloatTexture(const Transform &tex2world,
+		const TextureParams &tp) {
+	return new ScaleTexture<float, float>(tp.GetFloatTexture("tex1", 1.f),
+		tp.GetFloatTexture("tex2", 1.f));
+}
+
+template <class T,class U> inline Texture<SWCSpectrum> * ScaleTexture<T,U>::CreateSWCSpectrumTexture(const Transform &tex2world,
+		const TextureParams &tp) {
+	return new ScaleTexture<SWCSpectrum, SWCSpectrum>(
+		tp.GetSWCSpectrumTexture("tex1", RGBColor(1.f)),
+		tp.GetSWCSpectrumTexture("tex2", RGBColor(1.f)));
+}
+
+}//namespace lux
+
