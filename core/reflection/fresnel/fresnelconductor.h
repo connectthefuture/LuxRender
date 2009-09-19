9f78cd382a6164b6e9f9a4dffb9dadd6d7000cd0
David Bucciarelli <dade@ngi.it>
2009-09-19 13:05:10 +0200

    Fixed BUG 562: Luxconsole -s (Linux 64) fails to network render when material names contain spaces

---
+++ core/reflection/fresnel/fresnelconductor.h
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2009 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+#ifndef LUX_FRESNELCONDUCTOR_H
+#define LUX_FRESNELCONDUCTOR_H
+// fresnelconductor.h*
+#include "lux.h"
+#include "fresnel.h"
+
+namespace lux
+{
+
+class  FresnelConductor : public Fresnel {
+public:
+	// FresnelConductor Public Methods
+	FresnelConductor(const SWCSpectrum &e, const SWCSpectrum &kk)
+		: eta(e), k(kk) {
+	}
+	virtual ~FresnelConductor() { }
+	virtual void Evaluate(const TsPack *tspack, float cosi, SWCSpectrum *const f) const;
+	virtual float Index(const TsPack *tspack) const { return eta.Filter(tspack); }
+private:
+	// FresnelConductor Private Data
+	SWCSpectrum eta, k;
+};
+
+}//namespace lux
+
+#endif // LUX_FRESNELCONDUCTOR_H
+
---
+++ lights/point.h
@@ -0,0 +1,81 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2009 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+// point.cpp*
+#include "lux.h"
+#include "light.h"
+#include "shape.h"
+#include "scene.h"
+#include "texture.h"
+#include "sphericalfunction.h"
+
+namespace lux
+{
+
+// PointLight Declarations
+class PointLight : public Light {
+public:
+	// PointLight Public Methods
+	PointLight(const Transform &light2world, 
+		const boost::shared_ptr< Texture<SWCSpectrum> > L, float gain,
+		SampleableSphericalFunction *ssf);
+	virtual ~PointLight();
+	virtual bool IsDeltaLight() const { return true; }
+	virtual bool IsEnvironmental() const { return false; }
+	virtual SWCSpectrum Power(const TsPack *tspack, const Scene *) const;
+	virtual SWCSpectrum Sample_L(const TsPack *tspack, const Point &P, float u1, float u2, float u3,
+		Vector *wo, float *pdf, VisibilityTester *visibility) const;
+	virtual SWCSpectrum Sample_L(const TsPack *tspack, const Scene *scene, float u1, float u2,
+			float u3, float u4, Ray *ray, float *pdf) const;
+	virtual float Pdf(const Point &, const Vector &) const;
+	virtual float Pdf(const Point &p, const Normal &n,
+		const Point &po, const Normal &ns) const;
+	virtual bool Sample_L(const TsPack *tspack, const Scene *scene, float u1, float u2, float u3, BSDF **bsdf, float *pdf, SWCSpectrum *Le) const;
+	virtual bool Sample_L(const TsPack *tspack, const Scene *scene, const Point &p, const Normal &n,
+		float u1, float u2, float u3, BSDF **bsdf, float *pdf, float *pdfDirect,
+		VisibilityTester *visibility, SWCSpectrum *Le) const;
+	virtual SWCSpectrum Le(const TsPack *tspack, const Scene *scene, const Ray &r,
+		const Normal &n, BSDF **bsdf, float *pdf, float *pdfDirect) const;
+	
+	static Light *CreateLight(const Transform &light2world,
+		const ParamSet &paramSet, const TextureParams &tp);
+private:
+	/**
+	 * Return the emmitted radiance in the given diretion.
+	 *
+	 * @param tspack The thread specific pack.
+	 * @param w      The normalized direction in LOCAL coordinates.
+	 * 
+	 * @return The emmitted radiance.
+	 */
+	SWCSpectrum L(const TsPack *tspack, const Vector& w) const;
+	// PointLight Private Data
+	Point lightPos;
+	bool flipZ;
+	boost::shared_ptr< Texture<SWCSpectrum> > Lbase;
+	DifferentialGeometry dummydg;
+	float gain;
+	SampleableSphericalFunction *func;
+};
+
+}//namespace lux
+
