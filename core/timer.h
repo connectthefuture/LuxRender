1055a859152e7fa4db22735b7e7801f44f5a9d6e
David Bucciarelli <dade@ngi.it>
2009-11-07 16:45:19 +0100

    Feature 576: Dynamic Epsilon, added MachineEpsilon class (but it is not yet active)

---
+++ core/timer.h
@@ -0,0 +1,76 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2009 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+#ifndef LUX_TIMER_H
+#define LUX_TIMER_H
+// timer.h*
+#include "lux.h"
+#if defined ( WIN32 ) || defined(__CYGWIN__)
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+#if (_MSC_VER >= 1400)
+#include <stdio.h>
+#define snprintf _snprintf
+#endif
+// Timer Declarations
+class  Timer {
+public:
+	// Public Timer Methods
+	Timer();
+	~Timer();
+	
+	void Start();
+	void Stop();
+	void Reset();
+	
+	double Time();
+private:
+	double GetTime();
+	// Private Timer Data
+	double time0, elapsed;
+#if defined( IRIX ) || defined( IRIX64 )
+	// Private IRIX Timer Data
+	int fd;
+	unsigned long long counter64;
+	unsigned int counter32;
+	unsigned int cycleval;
+	
+	typedef unsigned long long iotimer64_t;
+	typedef unsigned int iotimer32_t;
+	volatile iotimer64_t *iotimer_addr64;
+	volatile iotimer32_t *iotimer_addr32;
+	
+	void *unmapLocation;
+	int unmapSize;
+#elif defined( WIN32 ) || defined(__CYGWIN__)
+	// Private Windows Timer Data
+	double one_over_frequency;
+	LARGE_INTEGER performance_counter, performance_frequency;
+#else
+	// Private UNIX Timer Data
+	struct timeval timeofday;
+#endif
+	bool running;
+};
+#endif // LUX_TIMER_H
---
+++ textures/gaussiantexture.h
@@ -0,0 +1,56 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2009 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+// gaussian.cpp*
+#include "lux.h"
+#include "texture.h"
+#include "gaussianspd.h"
+#include "paramset.h"
+
+namespace lux
+{
+
+// GaussianTexture Declarations
+class GaussianTexture : public Texture<SWCSpectrum> {
+public:
+	// GaussianTexture Public Methods
+	GaussianTexture(float m, float w, float r) : GSPD(m, w, r) { }
+	virtual ~GaussianTexture() { }
+	virtual SWCSpectrum Evaluate(const TsPack *tspack,
+		const DifferentialGeometry &) const {
+		return SWCSpectrum(tspack, &GSPD);
+	}
+	virtual float Y() const { return GSPD.Y(); }
+	virtual void SetPower(float power, float area) {
+		const float y = Y();
+		if (!(y > 0.f))
+			return;
+		GSPD.Scale(power / (area * M_PI * y));
+	}
+	static Texture<SWCSpectrum> *CreateSWCSpectrumTexture(const Transform &tex2world, const TextureParams &tp);
+
+private:
+	GaussianSPD GSPD;
+};
+
+}//namespace lux
+
