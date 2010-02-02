32d0efb9dd1aede788950ebc844200aa5f582e3e
Jean-Philippe Grimaldi <jeanphi@via.ecp.fr>
2010-02-02 21:05:40 +0100

    Update .hgignore
    Jeanphi

---
+++ core/reflection/fresnel/fresnelslick.cpp
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2009 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+// fresnelslick.cpp*
+#include "fresnelslick.h"
+#include "spectrum.h"
+
+using namespace lux;
+
+void FresnelSlick::Evaluate(const TsPack *tspack, float cosi, SWCSpectrum *const f) const {
+  *f = normal_incidence + (1.f - normal_incidence) * powf(1.f - cosi, 5.f);
+}
+
+FresnelSlick::FresnelSlick (float ni) {
+  normal_incidence = ni;
+}
+
