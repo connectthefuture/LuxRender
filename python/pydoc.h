77e70936f9698004d359e25a7ed2bdcc34e496af
Doug Hammond <Doug Hammond@localhost>
2010-05-03 19:09:21 +0100

    Working on pylux docsctrings, changed PyContext::getHistogramImage()

---
+++ python/pydoc.h
@@ -0,0 +1,282 @@
+// Python dosctrings for pylux Module
+// Try to keep within 80 columns
+// ref: http://epydoc.sourceforge.net/manual-epytext.html
+// -----------------------------------------------------------------------------
+
+const char * ds_pylux =
+"LuxRender Python Bindings\n\n"
+"Provides access to the LuxRender API in python\n\n"
+"TODO: Docstrings marked (+) need verification.";
+
+const char * ds_pylux_version =
+"Returns the pylux/LuxRender version";
+
+const char * ds_pylux_ThreadSignals =
+"Valid states for rendering threads";
+
+const char * ds_pylux_RenderingThreadInfo =
+"Container class for information about rendering threads";
+
+const char * ds_pylux_luxComponent =
+"(+) LuxRender Components available to modify at render-time";
+
+const char * ds_pylux_luxComponentParameters =
+"(+) Parameters of luxComponents available to modify at render time";
+
+const char * ds_pylux_RenderingServerInfo =
+"(+) Container class for information about rendering servers";
+
+const char * ds_pylux_errorHandler =
+"Specify an alternate error handler (logging) function for LuxRender engine\n"
+"output. By default the render engine will print to stdout";
+
+const char * ds_pylux_Context =
+"An instance of a LuxRender rendering context";
+
+const char * ds_pylux_Context_init =
+"Create a new rendering Context object with the given name";
+
+const char * ds_pylux_Context_accelerator =
+"Initialise geometry acceleration structure type. Valid types are\n"
+"- bruteforce\n"
+"- bvh\n"
+"- grid\n"
+"- qbvh\n"
+"- tabreckdtree (or kdtree)";
+
+const char * ds_pylux_Context_addServer =
+"Add a (remote) rendering server to the context";
+
+const char * ds_pylux_Context_addThread =
+"Add a local rendering thread to the context";
+
+const char * ds_pylux_Context_areaLightSource =
+"Attach a light source to the current geometry definition. (See: RiSpec 3.2 p.43)";
+
+const char * ds_pylux_Context_attributeBegin =
+"Begin a new Attribute scope.";
+
+const char * ds_pylux_Context_attributeEnd =
+"End an Attribute scope.";
+
+const char * ds_pylux_Context_camera =
+"Add a camera to the scene. Valid types are:\n"
+"- environment\n"
+"- orthographic\n"
+"- perspective\n"
+"- realistic";
+
+const char * ds_pylux_Context_cleanup =
+"Clean up and reset the renderer state after rendering.";
+
+const char * ds_pylux_Context_concatTransform =
+"Concatenate the given transformation onto the current transformation. The\n"
+"transformation is applied before all previously applied transformations, that\n"
+"is, before the current transformation. (Ref: RiSpec 3.2 p.56)";
+
+const char * ds_pylux_Context_coordSysTransform =
+"Replaces the current transformation matrix with the matrix that forms the named\n"
+"coordinate system. This permits objects to be placed directly into special or\n"
+"user-defined coordinate systems by their names. (Ref: RiSpec 3.2 p.57)";
+
+const char * ds_pylux_Context_coordinateSystem =
+"This function marks the coordinate system defined by the current transformation\n"
+"with the name space and saves it. (Ref: RiSpec 3.2 p.58)";
+
+const char * ds_pylux_Context_disableRandomMode =
+"Disables random mode in the renderer core.";
+
+const char * ds_pylux_Context_enableDebugMode =
+"Puts the renderer core into Debug mode.";
+
+const char * ds_pylux_Context_exit =
+"Stop the current rendering.";
+
+const char * ds_pylux_Context_exterior =
+"Sets the current Exterior volume shader. (Ref: RiSpec 3.2 p.48)";
+
+const char * ds_pylux_Context_film =
+"Initialise the Film for rendering. Valid types are:\n"
+"- fleximage.";
+
+const char * ds_pylux_Context_framebuffer =
+"Returns the current post-processed LDR framebuffer in RGB888 format as a list.\n"
+"It is advisable to call updateFramebuffer() before calling this function.";
+
+const char * ds_pylux_Context_getDefaultParameterValue =
+"";
+
+const char * ds_pylux_Context_getDefaultStringParameterValue =
+"";
+
+const char * ds_pylux_Context_getHistogramImage =
+"";
+
+const char * ds_pylux_Context_getNetworkServerUpdateInterval =
+"";
+
+const char * ds_pylux_Context_getOption =
+"";
+
+const char * ds_pylux_Context_getOptions =
+"";
+
+const char * ds_pylux_Context_getParameterValue =
+"";
+
+const char * ds_pylux_Context_getRenderingServersStatus =
+"";
+
+const char * ds_pylux_Context_getRenderingThreadsStatus =
+"";
+
+const char * ds_pylux_Context_getServerCount =
+"";
+
+const char * ds_pylux_Context_getStringParameterValue =
+"";
+
+const char * ds_pylux_Context_identity =
+"";
+
+const char * ds_pylux_Context_interior =
+"";
+
+const char * ds_pylux_Context_lightGroup =
+"";
+
+const char * ds_pylux_Context_lightSource =
+"";
+
+const char * ds_pylux_Context_loadFLM =
+"";
+
+const char * ds_pylux_Context_lookAt =
+"";
+
+const char * ds_pylux_Context_makeNamedMaterial =
+"";
+
+const char * ds_pylux_Context_makeNamedVolume =
+"";
+
+const char * ds_pylux_Context_material =
+"";
+
+const char * ds_pylux_Context_motionInstance =
+"";
+
+const char * ds_pylux_Context_namedMaterial =
+"";
+
+const char * ds_pylux_Context_objectBegin =
+"";
+
+const char * ds_pylux_Context_objectEnd =
+"";
+
+const char * ds_pylux_Context_objectInstance =
+"";
+
+const char * ds_pylux_Context_overrideResumeFLM =
+"";
+
+const char * ds_pylux_Context_parse =
+"Parse the given filename. If done asynchronously, control will pass\n"
+"immediately back to the python interpreter, otherwise this function blocks.";
+
+const char * ds_pylux_Context_pause =
+"";
+
+const char * ds_pylux_Context_pixelFilter =
+"";
+
+const char * ds_pylux_Context_portalShape =
+"";
+
+const char * ds_pylux_Context_removeServer =
+"";
+
+const char * ds_pylux_Context_removeThread =
+"";
+
+const char * ds_pylux_Context_reverseOrientation =
+"";
+
+const char * ds_pylux_Context_rotate =
+"";
+
+const char * ds_pylux_Context_sampler =
+"";
+
+const char * ds_pylux_Context_saveFLM =
+"";
+
+const char * ds_pylux_Context_scale =
+"";
+
+const char * ds_pylux_Context_setEpsilon =
+"";
+
+const char * ds_pylux_Context_setHaltSamplePerPixel =
+"";
+
+const char * ds_pylux_Context_setNetworkServerUpdateInterval =
+"";
+
+const char * ds_pylux_Context_setOption =
+"";
+
+const char * ds_pylux_Context_setParameterValue =
+"";
+
+const char * ds_pylux_Context_setStringParameterValue =
+"";
+
+const char * ds_pylux_Context_shape =
+"";
+
+const char * ds_pylux_Context_start =
+"";
+
+const char * ds_pylux_Context_statistics =
+"";
+
+const char * ds_pylux_Context_surfaceIntegrator =
+"";
+
+const char * ds_pylux_Context_texture =
+"";
+
+const char * ds_pylux_Context_transform =
+"";
+
+const char * ds_pylux_Context_transformBegin =
+"";
+
+const char * ds_pylux_Context_transformEnd =
+"";
+
+const char * ds_pylux_Context_translate =
+"";
+
+const char * ds_pylux_Context_updateFilmFromNetwork =
+"";
+
+const char * ds_pylux_Context_updateFramebuffer =
+"";
+
+const char * ds_pylux_Context_volume =
+"";
+
+const char * ds_pylux_Context_volumeIntegrator =
+"";
+
+const char * ds_pylux_Context_wait =
+"";
+
+const char * ds_pylux_Context_worldBegin =
+"";
+
+const char * ds_pylux_Context_worldEnd =
+"";
---
+++ textures/mix.h
@@ -0,0 +1,95 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2009 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+// mix.cpp*
+#include "lux.h"
+#include "spectrum.h"
+#include "texture.h"
+#include "color.h"
+#include "paramset.h"
+
+namespace lux
+{
+
+// MixTexture Declarations
+template <class T>
+class MixTexture : public Texture<T> {
+public:
+	// MixTexture Public Methods
+	MixTexture(boost::shared_ptr<Texture<T> > &t1,
+		boost::shared_ptr<Texture<T> > &t2,
+		boost::shared_ptr<Texture<float> > &amt) : tex1(t1), tex2(t2),
+		amount(amt) { }
+	virtual ~MixTexture() { }
+	virtual T Evaluate(const TsPack *tspack, const DifferentialGeometry &dg) const {
+		T t1 = tex1->Evaluate(tspack, dg), t2 = tex2->Evaluate(tspack, dg);
+		float amt = amount->Evaluate(tspack, dg);
+		return Lerp(amt, t1, t2);
+	}
+	virtual float Y() const { return Lerp(amount->Y(), tex1->Y(),
+		tex2->Y()); }
+	virtual float Filter() const { return Lerp(amount->Y(), tex1->Filter(),
+		tex2->Filter()); }
+	virtual void GetDuv(const TsPack *tspack,
+		const DifferentialGeometry &dg, float delta,
+		float *du, float *dv) const {
+		float dua, dva, du1, dv1, du2, dv2;
+		amount->GetDuv(tspack, dg, delta, &dua, &dva);
+		tex1->GetDuv(tspack, dg, delta, &du1, &dv1);
+		tex2->GetDuv(tspack, dg, delta, &du2, &dv2);
+		float a = amount->Evaluate(tspack, dg);
+		float d = tex2->EvalFloat(tspack, dg) -
+			tex1->EvalFloat(tspack, dg);
+		*du = Lerp(a, du1, du2) + d * dua;
+		*dv = Lerp(a, dv1, dv2) + d * dva;
+	}
+	virtual void SetIlluminant() {
+		// Update sub-textures
+		tex1->SetIlluminant();
+		tex2->SetIlluminant();
+	}
+	static Texture<float> * CreateFloatTexture(const Transform &tex2world, const ParamSet &tp);
+	static Texture<SWCSpectrum> * CreateSWCSpectrumTexture(const Transform &tex2world, const ParamSet &tp);
+private:
+	boost::shared_ptr<Texture<T> > tex1, tex2;
+	boost::shared_ptr<Texture<float> > amount;
+};
+
+// MixTexture Method Definitions
+template <class T> Texture<float> * MixTexture<T>::CreateFloatTexture(const Transform &tex2world,
+	const ParamSet &tp) {
+	boost::shared_ptr<Texture<float> > tex1(tp.GetFloatTexture("tex1", 0.f)),
+		tex2(tp.GetFloatTexture("tex2", 1.f));
+	boost::shared_ptr<Texture<float> > amount(tp.GetFloatTexture("amount", .5f));
+	return new MixTexture<float>(tex1, tex2, amount);
+}
+
+template <class T> Texture<SWCSpectrum> * MixTexture<T>::CreateSWCSpectrumTexture(const Transform &tex2world,
+	const ParamSet &tp) {
+	boost::shared_ptr<Texture<SWCSpectrum> > tex1(tp.GetSWCSpectrumTexture("tex1", RGBColor(0.f))),
+		tex2(tp.GetSWCSpectrumTexture("tex2", RGBColor(1.f)));
+	boost::shared_ptr<Texture<float> > amount(tp.GetFloatTexture("amount", .5f));
+	return new MixTexture<SWCSpectrum>(tex1, tex2, amount);
+}
+
+}//namespace lux
+
