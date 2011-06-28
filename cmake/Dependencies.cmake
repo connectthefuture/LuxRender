ee21fbe30dbf6440c5fb031e4afa569b56774b2d
neo2068 <neo2068@localhost>
2011-06-28 15:11:41 +0200

    Fixed missing iterator variable for used opencl devices log output

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,229 @@
+###########################################################################
+#   Copyright (C) 1998-2011 by authors (see AUTHORS.txt )                 #
+#                                                                         #
+#   This file is part of Lux.                                             #
+#                                                                         #
+#   Lux is free software; you can redistribute it and/or modify           #
+#   it under the terms of the GNU General Public License as published by  #
+#   the Free Software Foundation; either version 3 of the License, or     #
+#   (at your option) any later version.                                   #
+#                                                                         #
+#   Lux is distributed in the hope that it will be useful,                #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#   GNU General Public License for more details.                          #
+#                                                                         #
+#   You should have received a copy of the GNU General Public License     #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>. #
+#                                                                         #
+#   Lux website: http://www.luxrender.net                                 #
+###########################################################################
+
+#############################################################################
+#############################################################################
+##########################      Find LuxRays       ##########################
+#############################################################################
+#############################################################################
+IF(APPLE)
+	FIND_PATH(LUXRAYS_INCLUDE_DIRS NAMES luxrays/luxrays.h PATHS ${OSX_DEPENDENCY_ROOT}/include/LuxRays)
+	FIND_LIBRARY(LUXRAYS_LIBRARY libluxrays.a ${OSX_DEPENDENCY_ROOT}/lib/LuxRays)
+ELSE(APPLE)
+	FIND_PATH(LUXRAYS_INCLUDE_DIRS NAMES luxrays/luxrays.h PATHS ../luxrays/include)
+	FIND_LIBRARY(LUXRAYS_LIBRARY luxrays ../luxrays/lib)
+ENDIF(APPLE)
+
+IF (LUXRAYS_INCLUDE_DIRS AND LUXRAYS_LIBRARY)
+	MESSAGE(STATUS "LuxRays include directory: " ${LUXRAYS_INCLUDE_DIRS})
+	MESSAGE(STATUS "LuxRays library directory: " ${LUXRAYS_LIBRARY})
+	INCLUDE_DIRECTORIES(SYSTEM ${LUXRAYS_INCLUDE_DIRS})
+ELSE (LUXRAYS_INCLUDE_DIRS AND LUXRAYS_LIBRARY)
+	MESSAGE(FATAL_ERROR "LuxRays not found.")
+ENDIF (LUXRAYS_INCLUDE_DIRS AND LUXRAYS_LIBRARY)
+
+
+#############################################################################
+#############################################################################
+###########################      Find OpenCL       ##########################
+#############################################################################
+#############################################################################
+
+if(LUXRAYS_DISABLE_OPENCL)
+	SET(OCL_LIBRARY "")
+else(LUXRAYS_DISABLE_OPENCL)
+	FIND_PATH(OPENCL_INCLUDE_DIRS NAMES CL/cl.hpp OpenCL/cl.hpp PATHS /usr/src/opencl-sdk/include /usr/local/cuda/include)
+	FIND_LIBRARY(OPENCL_LIBRARY OpenCL /usr/src/opencl-sdk/lib/x86_64)
+
+	IF (OPENCL_INCLUDE_DIRS AND OPENCL_LIBRARY)
+		MESSAGE(STATUS "OpenCL include directory: " ${OPENCL_INCLUDE_DIRS})
+		MESSAGE(STATUS "OpenCL library directory: " ${OPENCL_LIBRARY})
+		INCLUDE_DIRECTORIES(SYSTEM ${OPENCL_INCLUDE_DIRS})
+	ELSE (OPENCL_INCLUDE_DIRS AND OPENCL_LIBRARY)
+		MESSAGE(FATAL_ERROR "OpenCL not found, try to compile with LUXRAYS_DISABLE_OPENCL=ON")
+	ENDIF (OPENCL_INCLUDE_DIRS AND OPENCL_LIBRARY)
+endif(LUXRAYS_DISABLE_OPENCL)
+
+
+#############################################################################
+#############################################################################
+###########################      Find OpenGL       ##########################
+#############################################################################
+#############################################################################
+
+FIND_PACKAGE(OpenGL)
+IF (OPENGL_FOUND)
+	message(STATUS "OpenGL include directory: " "${OPENGL_INCLUDE_DIRS}")
+	message(STATUS "OpenGL library: " "${OPENGL_LIBRARY}")
+	INCLUDE_DIRECTORIES(SYSTEM ${OPENGL_INCLUDE_DIRS})
+ELSE (OPENGL_FOUND)
+	MESSAGE(FATAL_ERROR "OpenGL not found.")
+ENDIF (OPENGL_FOUND)
+
+
+#############################################################################
+#############################################################################
+###########################      Find BISON       ###########################
+#############################################################################
+#############################################################################
+
+FIND_PACKAGE(BISON REQUIRED)
+IF (NOT BISON_FOUND)
+	MESSAGE(FATAL_ERROR "bison not found - aborting")
+ENDIF (NOT BISON_FOUND)
+
+
+#############################################################################
+#############################################################################
+###########################      Find FLEX        ###########################
+#############################################################################
+#############################################################################
+
+FIND_PACKAGE(FLEX REQUIRED)
+IF (NOT FLEX_FOUND)
+	MESSAGE(FATAL_ERROR "flex not found - aborting")
+ENDIF (NOT FLEX_FOUND)
+
+
+
+#############################################################################
+#############################################################################
+########################### BOOST LIBRARIES SETUP ###########################
+#############################################################################
+#############################################################################
+
+IF(APPLE)
+	SET(BOOST_ROOT ${OSX_DEPENDENCY_ROOT})
+ENDIF(APPLE)
+SET(Boost_MINIMUM_VERSION "1.43")
+SET(Boost_ADDITIONAL_VERSIONS "1.46.2" "1.46.1" "1.46.0" "1.46" "1.45.0" "1.45" "1.44.0" "1.44" "1.43.0" "1.43")
+SET(Boost_COMPONENTS thread program_options filesystem serialization iostreams regex system)
+IF(WIN32)
+	SET(Boost_COMPONENTS ${Boost_COMPONENTS} zlib)
+	SET(Boost_USE_STATIC_LIBS ON)
+	SET(Boost_USE_MULTITHREADED ON)
+	SET(Boost_USE_STATIC_RUNTIME OFF)
+ENDIF(WIN32)
+
+FIND_PACKAGE(Boost ${Boost_MINIMUM_VERSION} COMPONENTS python REQUIRED)
+
+SET(Boost_python_FOUND ${Boost_FOUND})
+SET(Boost_python_LIBRARIES ${Boost_LIBRARIES})
+SET(Boost_FOUND)
+SET(Boost_LIBRARIES)
+
+FIND_PACKAGE(Boost ${Boost_MINIMUM_VERSION} COMPONENTS ${Boost_COMPONENTS} REQUIRED)
+
+IF(Boost_FOUND)
+	MESSAGE(STATUS "Boost library directory: " ${Boost_LIBRARY_DIRS})
+	MESSAGE(STATUS "Boost include directory: " ${Boost_INCLUDE_DIRS})
+
+	# Don't use latest boost versions interfaces
+	ADD_DEFINITIONS(-DBOOST_FILESYSTEM_VERSION=2)
+	INCLUDE_DIRECTORIES(SYSTEM ${Boost_INCLUDE_DIRS})
+ELSE(Boost_FOUND)
+	MESSAGE(FATAL_ERROR "Could not find Boost")
+ENDIF(Boost_FOUND)
+
+
+#############################################################################
+#############################################################################
+######################### OPENEXR LIBRARIES SETUP ###########################
+#############################################################################
+#############################################################################
+
+# !!!!freeimage needs headers from or matched with freeimage !!!!
+IF(APPLE)
+	FIND_PATH(OPENEXR_INCLUDE_DIRS
+		OpenEXRConfig.h
+		PATHS
+		${OSX_DEPENDENCY_ROOT}/include/OpenEXR
+		NO_DEFAULT_PATH
+	)
+ELSE(APPLE)
+	FIND_PATH(OPENEXR_INCLUDE_DIRS
+		ImfXdr.h
+		PATHS
+		/usr/local/include/OpenEXR
+		/usr/include/OpenEXR
+		/sw/include/OpenEXR
+		/opt/local/include/OpenEXR
+		/opt/csw/include/OpenEXR
+		/opt/include/OpenEXR
+	)
+	MESSAGE(STATUS "OpenEXR include directory: " ${OPENEXR_INCLUDE_DIRS})
+ENDIF(APPLE)
+
+#############################################################################
+#############################################################################
+########################### PNG   LIBRARIES SETUP ###########################
+#############################################################################
+#############################################################################
+# - Find the native PNG includes and library
+#
+# This module defines
+#  PNG_INCLUDE_DIR, where to find png.h, etc.
+#  PNG_LIBRARIES, the libraries to link against to use PNG.
+#  PNG_DEFINITIONS - You should ADD_DEFINITONS(${PNG_DEFINITIONS}) before compiling code that includes png library files.
+#  PNG_FOUND, If false, do not try to use PNG.
+# also defined, but not for general use are
+#  PNG_LIBRARY, where to find the PNG library.
+# None of the above will be defined unles zlib can be found.
+# PNG depends on Zlib
+IF(APPLE)
+	FIND_PATH(PNG_INCLUDE_DIR
+		pngconf.h
+		PATHS
+		${OSX_DEPENDENCY_ROOT}/include
+		NO_DEFAULT_PATH
+	)
+ELSE(APPLE)
+	FIND_PACKAGE(PNG)
+	IF(NOT PNG_FOUND)
+		MESSAGE(STATUS "Warning : could not find PNG - building without png support")
+	ENDIF(NOT PNG_FOUND)
+ENDIF(APPLE)
+
+
+
+#############################################################################
+#############################################################################
+###########################   FREEIMAGE LIBRARIES    ########################
+#############################################################################
+#############################################################################
+
+	FIND_PACKAGE(FreeImage REQUIRED)
+
+	IF(FREEIMAGE_FOUND)
+		MESSAGE(STATUS "FreeImage library directory: " ${FREEIMAGE_LIBRARIES})
+		MESSAGE(STATUS "FreeImage include directory: " ${FREEIMAGE_INCLUDE_PATH})
+	ELSE(FREEIMAGE_FOUND)
+		MESSAGE(FATAL_ERROR "Could not find FreeImage")
+	ENDIF(FREEIMAGE_FOUND)
+
+#############################################################################
+#############################################################################
+############################ THREADING LIBRARIES ############################
+#############################################################################
+#############################################################################
+
+	FIND_PACKAGE(Threads REQUIRED)
+
