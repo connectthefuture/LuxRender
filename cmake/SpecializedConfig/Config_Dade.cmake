ef3d4be8c2a077b7341ffa0956fb7118ec4c8cdb
neo2068 <neo2068@web.de>
2014-02-10 18:43:20 +0100

    Add missing changes to blender clouds class

---
+++ cmake/SpecializedConfig/Config_Dade.cmake
@@ -0,0 +1,26 @@
+
+###########################################################################
+#
+# Configuration
+#
+###########################################################################
+
+#cmake -DLUX_CUSTOM_CONFIG=cmake/SpecializedConfig/Config_Dade.cmake .
+
+MESSAGE(STATUS "Using Dade Configuration settings")
+
+SET(LuxRays_HOME		"../luxrays")
+
+SET(OPENCL_SEARCH_PATH		"$ENV{ATISTREAMSDKROOT}")
+SET(OPENCL_INCLUDE_DIR		"${OPENCL_SEARCH_PATH}/include")
+#SET(OPENCL_LIBRARYDIR		"${OPENCL_SEARCH_PATH}/lib/x86_64")
+set(OPENIMAGEIO_ROOT_DIR    "/home/david/projects/luxrender-dev/oiio-RB-1.3/dist/linux64")
+set(OPENEXR_ROOT            "/usr/local")
+
+#SET(BOOST_SEARCH_PATH		"/home/david/projects/luxrender-dev/boost_1_43_0")
+#SET(BOOST_LIBRARYDIR		"${BOOST_SEARCH_PATH}")
+#SET(BOOST_python_LIBRARYDIR	"${BOOST_SEARCH_PATH}")
+#SET(BOOST_ROOT			"${BOOST_SEARCH_PATH}")
+
+#SET(CMAKE_BUILD_TYPE "Debug")
+SET(CMAKE_BUILD_TYPE "Release")
